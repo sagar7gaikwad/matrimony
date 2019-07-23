58e1318cb38da6f96cada218faca15ad4be4c61e
sagar7gaikwad <sagar7gaikwad@gmail.com>
2019-07-23 11:30:17 +0530

    initial commit

---
+++ src/main/java/com/hcl/matrimonyapp/MatrimonyappApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.matrimonyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrimonyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrimonyappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcl/matrimonyapp/MatrimonyappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hcl.matrimonyapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MatrimonyappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
