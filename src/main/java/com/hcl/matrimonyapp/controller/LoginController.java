4e3f7083d78cc521e40c38a821a5c9e6aeb35a7d
pracheerjain <pracheer2002@gmail.com>
2019-07-23 17:17:57 +0530

    Login codes and Junits

--- src/main/java/com/hcl/matrimonyapp/controller/LoginController.java
+++ src/main/java/com/hcl/matrimonyapp/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.hcl.matrimonyapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hcl.matrimonyapp.dto.LoginDTO;
 import com.hcl.matrimonyapp.dto.UserProfileDTO;
+import com.hcl.matrimonyapp.exception.ApplicationException;
 import com.hcl.matrimonyapp.service.LoginService;
-
 
 /**
  * @author Administrator
@@ -27,21 +28,23 @@ public class LoginController {
 	/**
 	 * @param loginDTO
 	 * @return object of ResponseEntity
+	 * @throws ApplicationException
 	 */
 	@PostMapping("")
-	public ResponseEntity<Object> login(@RequestBody LoginDTO loginDTO) {
+	public ResponseEntity<Object> login(@RequestBody LoginDTO loginDTO) throws ApplicationException {
 
 		UserProfileDTO userProfileDTO = null;
-		if (!isNullOrEmpty(loginDTO) && !isNullOrEmpty(loginDTO.getUserId())
-				&& !isNullOrEmpty(loginDTO.getPassword())) {
-			userProfileDTO = loginService.getUserProfile(loginDTO.getUserId(), loginDTO.getPassword());
-			if (!isNullOrEmpty(userProfileDTO))
-				return new ResponseEntity<>(userProfileDTO, HttpStatus.OK);
-		}
-		return new ResponseEntity<>("Please enter valid credentials.", HttpStatus.BAD_REQUEST);
+		validate(loginDTO);
+		userProfileDTO = loginService.getUserProfile(loginDTO.getUserId(), loginDTO.getPassword());
+		if (null == userProfileDTO)
+			throw new ApplicationException("Please enter valid credentials.");
+		return new ResponseEntity<>(userProfileDTO, HttpStatus.OK);
+		
 	}
 
-	private boolean isNullOrEmpty(Object object) {
-		return (null == object || " ".equals(object));
+	private void validate(LoginDTO loginDTO) throws ApplicationException {
+		if (null == loginDTO || null == loginDTO.getUserId() || StringUtils.isEmpty(loginDTO.getUserId())
+				|| null == loginDTO.getPassword() || StringUtils.isEmpty(loginDTO.getPassword()))
+			throw new ApplicationException("Please enter valid credentials.");
 	}
 }
--- src/test/java/com/hcl/matrimonyapp/controller/LoginControllerTest.java
+++ src/test/java/com/hcl/matrimonyapp/controller/LoginControllerTest.java
@@ -12,6 +12,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import com.hcl.matrimonyapp.dto.LoginDTO;
 import com.hcl.matrimonyapp.dto.UserProfileDTO;
+import com.hcl.matrimonyapp.exception.ApplicationException;
 import com.hcl.matrimonyapp.service.LoginService;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -19,37 +20,45 @@ public class LoginControllerTest {
 
 	@InjectMocks
 	LoginController loginController;
-	
+
 	@Mock
 	LoginService loginService;
-	
+
 	LoginDTO loginDTO;
-	
+
 	@Before
 	public void setUp() {
 		loginDTO = new LoginDTO();
 	}
-	
+
 	@Test
-	public void loginIfCredentialDetailsAreCorrect() {
+	public void loginIfCredentialDetailsAreCorrect() throws ApplicationException {
 		loginDTO.setPassword("Jatin@123");
 		loginDTO.setUserId(1L);
 		UserProfileDTO userProfileDTO = new UserProfileDTO();
 		when(loginService.getUserProfile(loginDTO.getUserId(), loginDTO.getPassword())).thenReturn(userProfileDTO);
 		Assert.assertNotNull(loginController.login(loginDTO));
 	}
-	
-	@Test
-	public void loginIfPasswordIsNull() {
+
+	@Test(expected = ApplicationException.class)
+	public void loginIfPasswordIsNull() throws ApplicationException {
 		loginDTO.setPassword(null);
 		loginDTO.setUserId(1L);
-		Assert.assertNotNull(loginController.login(loginDTO));
+		loginController.login(loginDTO);
 	}
-	
-	@Test
-	public void loginIfUserIdIsNull() {
+
+	@Test(expected = ApplicationException.class)
+	public void loginIfUserIdIsNull() throws ApplicationException {
 		loginDTO.setPassword("Jatin@123");
 		loginDTO.setUserId(null);
-		Assert.assertNotNull(loginController.login(loginDTO));
+		loginController.login(loginDTO);
+	}
+	
+	@Test(expected = ApplicationException.class)
+	public void loginIfCredentialsAreIncorrect() throws ApplicationException {
+		loginDTO.setPassword("Jtin");
+		loginDTO.setUserId(1L);
+		when(loginService.getUserProfile(loginDTO.getUserId(), loginDTO.getPassword())).thenReturn(null);
+		loginController.login(loginDTO);
 	}
 }
