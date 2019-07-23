f7cf6af9ff32b204d48b875ff952ec02f017bd3e
shivanjali.kanase <kanaseshivanjali@gmail.com>
2019-07-23 14:08:26 +0530

    added entities.

--- src/main/java/com/hcl/matrimonyapp/dto/UserProfileDTO.java
+++ src/main/java/com/hcl/matrimonyapp/dto/UserProfileDTO.java
@@ -3,13 +3,6 @@ package com.hcl.matrimonyapp.dto;
 import java.time.LocalDate;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
 import com.hcl.matrimonyapp.entity.Favourites;
 
 import lombok.Data;
@@ -17,65 +10,56 @@ import lombok.Data;
 @Data
 public class UserProfileDTO {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "user_id")
-	private long userId;
+	//private long userId;
 
-	@Column(name = "name")
 	private String name;
 
-	@Column(name = "age")
 	private long age;
 
-	@Column(name = "occupation")
 	private String occupation;
 
-	@Column(name = "gender")
 	private String gender;
 
-	@Column(name = "complexion")
 	private String complexion;
 
-	@Column(name = "height")
 	private double height;
 
-	@Column(name = "weight")
 	private double weight;
 
-	@Column(name = "horoscopeDetails")
 	private String horoscopeDetails;
 
-	@Column(name = "dob")
 	private LocalDate dob;
 
-	@Column(name = "caste")
 	private String caste;
 
-	@Column(name = "education")
 	private String education;
 
-	@Column(name = "blood_grp")
 	private String bloodGrp;
 
-	@Column(name = "password")
 	private String password;
 
-	@Column(name = "username")
 	private String username;
 
-	// @Column(name = "my_fav_list")
-	@OneToMany(mappedBy = "myUserProfile", cascade = CascadeType.ALL)
 	private List<Favourites> myFavList;
 
-	// @Column(name = "fav_me_list")
-	@OneToMany(mappedBy = "likedUserProfile", cascade = CascadeType.ALL)
 	private List<Favourites> favMeList;
 
-	@Column(name = "current_addr")
 	private String currentAddr;
 
-	@Column(name = "native_addr")
 	private String navtiveAddr;
+
+	@Override
+	public String toString() {
+		final int maxLen = 10;
+		return "UserProfileDTO [name=" + name + ", age=" + age + ", occupation=" + occupation + ", gender=" + gender
+				+ ", complexion=" + complexion + ", height=" + height + ", weight=" + weight + ", horoscopeDetails="
+				+ horoscopeDetails + ", dob=" + dob + ", caste=" + caste + ", education=" + education + ", bloodGrp="
+				+ bloodGrp + ", username=" + username + ", myFavList="
+				+ (myFavList != null ? myFavList.subList(0, Math.min(myFavList.size(), maxLen)) : null) + ", favMeList="
+				+ (favMeList != null ? favMeList.subList(0, Math.min(favMeList.size(), maxLen)) : null)
+				+ ", currentAddr=" + currentAddr + ", navtiveAddr=" + navtiveAddr + "]";
+	}
+	
+	
 
 }
