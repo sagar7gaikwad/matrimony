69c22dfb88b786b326c904a3bac35c22880eaefa
shivanjali.kanase <kanaseshivanjali@gmail.com>
2019-07-23 17:59:56 +0530

    added test

--- src/test/java/com/hcl/matrimonyapp/controller/RegisterControllerTest.java
+++ src/test/java/com/hcl/matrimonyapp/controller/RegisterControllerTest.java
@@ -1,5 +1,7 @@
 package com.hcl.matrimonyapp.controller;
 
+import java.time.LocalDate;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,7 +11,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.hcl.matrimonyapp.dto.UserProfileDTO;
+import com.hcl.matrimonyapp.exception.ApplicationException;
 import com.hcl.matrimonyapp.serviceimpl.RegisterServiceImpl;
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class RegisterControllerTest {
@@ -28,15 +32,33 @@ public class RegisterControllerTest {
 	}
 	
 	@Test
-	public void testRegisterUser() {
+	public void testRegisterUser() throws ApplicationException {
 		//Mockito.when(mortgageServiceMock.getOffers(userRequest)).thenReturn(offerList);
 		//ResponseEntity<?> calculateMortgage = mortgageController.calculateMortgage(userRequest);
 		Mockito.when(registerServiceImplMock.registerUser(Mockito.any(UserProfileDTO.class))).thenReturn("");
+		registerController.registerUser(userProfileDTO);
 		
 	}
 	
 	private UserProfileDTO createUserDTO() {
-		return null;
+		UserProfileDTO userProfileDTO = new UserProfileDTO();
+		userProfileDTO.setBloodGrp("A+");
+		userProfileDTO.setCaste("Maratha");
+		userProfileDTO.setComplexion("Fair");
+		userProfileDTO.setCurrentAddr("Pune");
+		userProfileDTO.setDob(LocalDate.of(1992, 05, 22));
+		userProfileDTO.setEducation("MTech");
+		userProfileDTO.setGender("Female");
+		userProfileDTO.setHeight(5.4);
+		userProfileDTO.setHoroscopeDetails("capricorn");
+		userProfileDTO.setName("Shivanjali");
+		userProfileDTO.setNavtiveAddr("Pune");
+		userProfileDTO.setOccupation("Engineer");
+		userProfileDTO.setPassword("shiv");
+		userProfileDTO.setUsername("shiv");
+		userProfileDTO.setWeight(57.0);
+		
+		return userProfileDTO;
 	}
 	
 }
