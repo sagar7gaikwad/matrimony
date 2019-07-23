2e4b1b5bd8a7064c1c3a7891501c5cc833183d91
shivanjali.kanase <kanaseshivanjali@gmail.com>
2019-07-23 17:44:44 +0530

    added test

--- src/test/java/com/hcl/matrimonyapp/controller/RegisterControllerTest.java
+++ src/test/java/com/hcl/matrimonyapp/controller/RegisterControllerTest.java
@@ -7,7 +7,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.http.ResponseEntity;
 
 import com.hcl.matrimonyapp.dto.UserProfileDTO;
 import com.hcl.matrimonyapp.serviceImpl.RegisterServiceImpl;
@@ -32,13 +31,12 @@ public class RegisterControllerTest {
 	public void testRegisterUser() {
 		//Mockito.when(mortgageServiceMock.getOffers(userRequest)).thenReturn(offerList);
 		//ResponseEntity<?> calculateMortgage = mortgageController.calculateMortgage(userRequest);
-		Mockito.when(registerServiceImplMock.registerUser(Mockito.any(UserProfileDTO.class))).thenReturn("Welcome to HCL Matrimony...\nHello " + savedUser.getName()
-				+ ", you are succesfully registerd.\nYour login id is : " + savedUser.getUserId());
+		Mockito.when(registerServiceImplMock.registerUser(Mockito.any(UserProfileDTO.class))).thenReturn("");
 		
 	}
 	
 	private UserProfileDTO createUserDTO() {
-		
+		return null;
 	}
 	
 }
